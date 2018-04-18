0c052ad10af3e7b8a666a852314160dbea2ac8ac
sztyup <sztyup@gmail.com>
2018-04-18 13:44:39 +0200

    PS-2018.1.1 <sztyup@sztyup-pc Create baseRefactoring.xml, markdown.xml, IntelliLang.xml, ide.general.xml, project.default.xml, diff.xml, Default copy.xml, applicationLibraries.xml, Default.xml, keymap.xml, find.xml, ui.lnf.xml, php.xml, colors.scheme.xml, git.xml, PHP Getter Method.php, Default.xml, parameter.hints.xml, laf.xml, intentionSettings.xml, vcs.xml, PHP Setter Method.php, databaseSettings.xml, editor.xml, usageView.xml, webServers.xml, debugger.xml

---
+++ fileTemplates/code/PHP Getter Method.php
@@ -0,0 +1,11 @@
+/**
+ * @return ${TYPE_HINT}
+ */
+public ${STATIC} function ${GET_OR_IS}${NAME}()
+{
+#if (${STATIC} == "static")
+    return self::$${FIELD_NAME};
+#else
+    return $this->${FIELD_NAME};
+#end
+}
